84b6e0ac62f197e6ddb2fc3b190f205f98a7f9d5
cooper <lyt3131@163.com>
2023-11-28 12:26:20 +0800

    file down

--- openresty/docker_build.sh
+++ openresty/docker_build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker buildx build . --platform linux/amd64,linux/arm64 --push -t coopersoft/seaweedfs-gateway:1.0.0
+docker buildx build . --platform linux/amd64,linux/arm64 --push -t coopersoft/seaweedfs-gateway:1.0.1
